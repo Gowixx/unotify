218dddd6d797c4f43b62fca17d6266e272db3f7b
Dimitri Demarkus <57415348+Gowixx@users.noreply.github.com>
2023-05-31 11:23:48 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
 import unotify
-
+from unotify import urgencies
 ## Icons List
 ## > https://specifications.freedesktop.org/icon-naming-spec/latest/ar01s04.html
 
 unotify.notify(
-    title = 'Testing',
-    message = 'uNotify example.',
-    urgency = unotify.urgencies.NORMAL,
-    timeout = 10,
-    app_name = 'Testing',
-    app_icon = 'edit-undo'
+    title="Testing",
+    message="uNotify example.",
+    urgency=urgencies.NORMAL,
+    timeout=10,
+    app_name="Testing",
+    app_icon="edit-undo"
 )
